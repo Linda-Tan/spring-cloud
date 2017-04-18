0bb366bb5e9d025d66b3192b746c9379e49f37e9
Tony.li <tony.li@regs.com>
2017-04-18 10:56:29 +0800

    modified

--- user-service/src/main/java/com/demo/UserServiceApplication.java
+++ user-service/src/main/java/com/demo/UserServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class WebServiceApplication {
+public class UserServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebServiceApplication.class, args);
+		SpringApplication.run(UserServiceApplication.class, args);
 	}
 }
